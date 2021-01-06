995d372de9f93643d741aa4b236f32c2b24c0b74
gego144 <56304474+gego144@users.noreply.github.com>
2021-01-06 15:24:34 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+
+const app = express();
+const jsonParser = bodyParser.json()
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+// CHANGE THIS TO DATABASE LATER OF USERS
+const users = [];
+
+
+//listen for requests
+
+app.listen(3000);
+
+app.use(express.static(__dirname + '/public')); 
+
+
+app.get('/index.html', (req, res) => {
+    res.sendFile('./index.html', {root: __dirname});
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile('./login.html', {root: __dirname});
+})
+
+app.post('/login', urlencodedParser, (req,res)=>{
+    res.json(req.body.password);
+})  
+
+app.get('/signup.html', (req, res) => {
+    res.sendFile('./signup.html', {root: __dirname});
+})
+
+app.post('/signup', urlencodedParser, (req,res)=>{
+    
+
+})
+
+app.use((req, res) => {
+    res.sendFile('./404.html', {root: __dirname});
+})
